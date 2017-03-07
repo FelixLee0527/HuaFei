a85e0dec1a2908f99ce22503025d8eeac38636c5
FelixLee <475980305@qq.com>
2017-03-07 23:58:21 +0800

    init

---
+++ app/src/main/java/com/felix/huafei/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.felix.huafei.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.felix.huafei.R;
+
+/**
+ * Created by felix on 2017/3/7.
+ */
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
